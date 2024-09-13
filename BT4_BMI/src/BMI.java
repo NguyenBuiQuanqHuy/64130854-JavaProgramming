ef0c508cf4f78d35ac27ed781238ec9b788430ad
NguyenBuiQuanqHuy <2k4quanghuy@gmail.com>
2024-09-13 15:39:44 +0700

    Update BMI.java
    đã chỉnh sửa

--- BT4_BMI/src/BMI.java
+++ BT4_BMI/src/BMI.java
@@ -1,15 +1,40 @@
 import java.util.Scanner;
 
+import javax.security.auth.x500.X500Principal;
+
 public class BMI {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		float w,h,bmi;
 		Scanner testScanner= new Scanner(System.in);
-		System.out.print("Nhập cân nặng: ");
-		w=testScanner.nextFloat();
-		System.out.print("Nhập chiều cao: ");
-		h=testScanner.nextFloat();
+		while(true) {
+			try {
+				System.out.print("Nhập cân nặng: ");
+				w=testScanner.nextFloat();
+				if(w>=30 && w<=100) break;
+			}
+			catch (Exception e) {
+				// TODO: handle exception
+				System.out.print("Cân nặng không hợp lệ");
+			}
+			
+		}
+		
+		while(true) {
+			try {
+				System.out.print("Nhập chiều cao: ");
+				h=testScanner.nextFloat();
+				if(h>=1.0 && h<=2.2) break;
+			}
+			catch (Exception e) {
+				// TODO: handle exception
+				System.out.print("Chiều cao không hợp lệ");
+			}
+			
+		}
+		
+		
 		bmi=w/(h*h);
 		if(bmi<18.5) {
 			System.out.print(bmi + ": Thiếu cân");
