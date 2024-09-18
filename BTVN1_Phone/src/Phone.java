4fc292b83244148147b8fc17d11ac824e40f7be8
NguyenBuiQuanqHuy <2k4quanghuy@gmail.com>
2024-09-18 23:29:44 +0700

    Hoàn thành

--- BTVN1_Phone/src/Phone.java
+++ BTVN1_Phone/src/Phone.java
@@ -1,4 +1,8 @@
 
-public class Phone {
-
+public abstract class Phone {
+	abstract void insertPhone(String name, String phone);
+	abstract void removePhone(String name);
+	abstract void updatePhone(String name, String newphone);
+	abstract void searchPhone(String name);
+	abstract void sort();
 }
