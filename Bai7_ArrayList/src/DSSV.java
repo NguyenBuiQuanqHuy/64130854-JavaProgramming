d564f2ba478810f199ad4fcdf15fb44d0bf9c64a
NguyenBuiQuanqHuy <2k4quanghuy@gmail.com>
2024-09-24 22:52:59 +0700

    Hoàn Thành

--- Bai7_ArrayList/src/DSSV.java
+++ Bai7_ArrayList/src/DSSV.java
@@ -1,4 +1,83 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class DSSV {
+	private ArrayList<SinhVien> dSinhViens =new ArrayList<SinhVien>();
 
+	public DSSV(ArrayList<SinhVien> dSinhViens) {
+		super();
+		this.dSinhViens = dSinhViens;
+	}
+
+	public DSSV() {
+		super();
+	}
+	
+	public void ThemSV(SinhVien sv) {
+		this.dSinhViens.add(sv);
+	}
+	
+	public void inDS() {
+		for(SinhVien sv:dSinhViens) {
+			System.out.println(sv.toString());
+		}
+	}
+	
+	//Kiểm tra sinh viên có tồn tại ko
+	public void KtSinhVien(String id) {
+		int c=0;
+		for(SinhVien sv:dSinhViens) {
+			if(sv.getId().equals(id)) {
+				c=1;
+				break;
+			}
+			
+		}
+		if(c==1) {
+			System.out.println("SV tồn tại!");
+		}
+		else {
+			System.out.println("SV không tồn tại!");
+		}
+	}
+	
+	public void Xoa(String id) {
+		int c=0;
+		for(SinhVien sv:dSinhViens) {
+			if(sv.getId().equals(id)) {
+				dSinhViens.remove(sv);
+				c=1;
+				break;
+			}
+			
+		}
+		if(c==1) {
+			System.out.println("Xóa SV thành công!");
+		}
+		else {
+			System.out.println("Xóa SV không thành công!");
+		}
+	}
+	
+	// Tìm kiếm tất cả sinh viên có tên được nhập từ bàn phím
+	public void Timkiem(String ten) {
+		for(SinhVien sv:dSinhViens) {
+			if(sv.getHoten().equals(ten)) System.out.println(sv.toString());
+		}
+	}
+	
+	// Sắp xếp điểm sinh viên từ cao đến thấp
+	public void Sapxep() {
+		Collections.sort(this.dSinhViens,new Comparator<SinhVien>() {
+
+			@Override
+			public int compare(SinhVien sv1, SinhVien sv2) {
+				// TODO Auto-generated method stub
+				if(sv1.getDiemTB()>sv2.getDiemTB()) return -1;
+				if(sv1.getDiemTB()<sv2.getDiemTB()) return 1;
+				else return 0;
+			}
+		});
+	}
 }
--- Bai7_ArrayList/src/SinhVien.java
+++ Bai7_ArrayList/src/SinhVien.java
@@ -1,4 +1,52 @@
 
-public class SinhVien {
-
+public class SinhVien implements Comparable<SinhVien>{
+	private String id;
+	private String hoten;
+	private int namsinh;
+	private float diemTB;
+	
+	
+	
+	public SinhVien() {
+		super();
+	}
+	public SinhVien(String id) {
+		this.id = id;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getHoten() {
+		return hoten;
+	}
+	public void setHoten(String hoten) {
+		this.hoten = hoten;
+	}
+	public int getNamsinh() {
+		return namsinh;
+	}
+	public void setNamsinh(int namsinh) {
+		this.namsinh = namsinh;
+	}
+	public float getDiemTB() {
+		return diemTB;
+	}
+	public void setDiemTB(float diemTB) {
+		this.diemTB = diemTB;
+	}
+	@Override
+	public String toString() {
+		return "SinhVien [id=" + id + ", hoten=" + hoten + ", namsinh=" + namsinh + ", diemTB=" + diemTB + "]";
+	}
+	@Override
+	public int compareTo(SinhVien o) {
+		// TODO Auto-generated method stub
+		return this.id.compareTo(o.id);
+	}
+	
+	
+	
 }
