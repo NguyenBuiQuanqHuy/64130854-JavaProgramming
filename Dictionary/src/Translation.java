ffba792d3408a8e76e74c09d2820e48669391816
NguyenBuiQuanqHuy <2k4quanghuy@gmail.com>
2024-10-01 14:04:36 +0700

    Hoàn Thành

--- Dictionary/src/Translation.java
+++ Dictionary/src/Translation.java
@@ -1,9 +1,21 @@
+import java.util.HashMap;
 
 public class Translation {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		HashMap<String, String> dictionary =new HashMap<String, String>();
+		dictionary.put("Hello", "Xin chào");
+		dictionary.put("Map", "Bản đồ");
+		dictionary.put("Marketing", "Thị trường");
+		dictionary.put("Time", "Thời gian");
+		dictionary.put("Transform", "Biến hình");
+		dictionary.put("School", "Trường học");
+		dictionary.put("Exercise", "Bài tập");
+		dictionary.put("Computer", "Máy tính");
+		dictionary.put("Keyboard", "Bàn phím");
+		dictionary.put("Mouse", "Chuột");
+		System.out.print(dictionary);
 	}
 
 }
