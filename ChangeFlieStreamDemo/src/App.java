d1fa189d15edd2f304e6b81aa69b200290310975
NguyenBuiQuanqHuy <2k4quanghuy@gmail.com>
2024-10-01 15:12:52 +0700

    Hoàn thành

--- ChangeFlieStreamDemo/src/App.java
+++ ChangeFlieStreamDemo/src/App.java
@@ -1,4 +1,16 @@
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class App {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		FileWriter fWriter;
+		fWriter =new FileWriter("songque.txt");
+		fWriter.write("Sông quê nước chảy đôi bờ..");
+		fWriter.write("Để anh chín dại mười khờ.. thương .. em");
+		fWriter.flush();
+		fWriter.close();
+	}
 
 }
