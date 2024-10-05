37101f5733066dcbd78ebaddd095e5c6497fba8e
NguyenBuiQuanqHuy <2k4quanghuy@gmail.com>
2024-10-05 23:02:34 +0700

    TinhTong

--- TinhTong/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.tinhtong {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.tinhtong to javafx.fxml;
-    exports com.example.tinhtong;
-}
--- demo/src/main/java/com/example/demo/HelloApplication.java
+++ demo/src/main/java/com/example/demo/HelloApplication.java
@@ -1,4 +1,4 @@
-package com.example.tinhtong;
+package com.example.demo;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
