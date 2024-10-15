632a77b5aa16f9221e40bb30cd9d6f44aecad096
NguyenBuiQuanqHuy <2k4quanghuy@gmail.com>
2024-10-15 15:34:50 +0700

    Thêm

--- Maven/src/main/java/com/example/maven/Controller.java
+++ Maven/src/main/java/com/example/maven/Controller.java
@@ -1,4 +1,33 @@
 package com.example.maven;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import org.json.simple.JSONObject;
+
 public class Controller {
+    @FXML
+    TextField txtMaSV,txtName,txtDob;
+    @FXML
+    TextArea textAreaThongtin;
+    public void Add(){
+        String MaSV=txtMaSV.getText();
+        String hoten=txtName.getText();
+        String DoB=txtDob.getText();
+
+        JSONObject jsonStudent =new JSONObject();
+        jsonStudent.put("Mã SV",MaSV);
+        jsonStudent.put("Họ tên SV",hoten);
+        jsonStudent.put("Năm sinh",DoB);
+        textAreaThongtin.setText(jsonStudent.toJSONString());
+    }
+
+    public void Reload(ActionEvent event){
+
+    }
+
+    public void Save(ActionEvent event){
+
+    }
 }
--- Maven/src/main/java/module-info.java
+++ Maven/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.maven {
     requires javafx.controls;
     requires javafx.fxml;
+    requires json.simple;
 
 
     opens com.example.maven to javafx.fxml;
