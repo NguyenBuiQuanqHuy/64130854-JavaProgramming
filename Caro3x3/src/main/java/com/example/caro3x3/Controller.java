821b2eafb9329ab95bd0bf91d39b012c601af0b2
NguyenBuiQuanqHuy <2k4quanghuy@gmail.com>
2024-10-18 22:06:05 +0700

    Caro

---
+++ Caro3x3/src/main/java/com/example/caro3x3/Controller.java
@@ -0,0 +1,19 @@
+package com.example.caro3x3;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class Controller {
+    Boolean turn=true;
+    @FXML Button btn1;
+    @FXML Button btn2;
+    @FXML Button btn3;
+    @FXML Button btn4;
+    @FXML Button btn5;
+    @FXML Button btn6;
+    @FXML Button btn7;
+    @FXML Button btn8;
+    @FXML Button btn9;
+
+
+}
